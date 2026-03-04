61dad3e270afbc96a58d890246d1d75121f3d6ed
“NGuy2919” <Aekmongkhon.p@kkumail.com>
2026-03-04 23:07:30 +0700

    add rating content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Component/Navbar";
 import Home from "./Page/Home";
-import AboutUs from "./page/AboutUs";
+import AboutUs from "./Page/AboutUs";
 import Login from "./Page/Login";
+import Profile from "./Page/Profile";
+// add other pages as needed
 
 function App() {
 
   return (
-    <>
-      <Home />
-      {/* <AboutUs /> */}
-      {/* <Login /> */}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Home />} /> {/* or a dedicated Course page */}
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        {/* add additional <Route> entries for other pages */}
+      </Routes>
+    </Router>
   );
 }
 
